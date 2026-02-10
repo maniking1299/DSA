f627c0983240e4ce562c97e21d4386ee7786f03e
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-02-10 19:11:54 +0530

    Time: 3 ms (99.32%) | Memory: 175.8 MB (25.57%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,33 +1,54 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int pos[] = new int[nums.length/2];
-        int neg[] = new int[nums.length/2];
-        int j=0;
-        int k=0;
 
-        for(int i=0 ; i<nums.length ;i++){
-            if(nums[i]<0){
-                neg[j] = nums[i];
-                j++;
+        int res[] = new int[nums.length];
+        int positive = 0;
+        int negative = 1;
+
+        for(int i:nums){
+            if(i<0){
+                res[negative] = i;
+                negative +=2;
             }else{
-
-            pos[k] = nums[i];
-            k++;
+                res[positive] = i;
+                positive +=2;
             }
+        }
+
+    return res;
+
+
+
+
+
+        // int pos[] = new int[nums.length/2];
+        // int neg[] = new int[nums.length/2];
+        // int j=0;
+        // int k=0;
+
+        // for(int i=0 ; i<nums.length ;i++){
+        //     if(nums[i]<0){
+        //         neg[j] = nums[i];
+        //         j++;
+        //     }else{
+
+        //     pos[k] = nums[i];
+        //     k++;
+        //     }
             
-        }
-        j=0;
-        k=0;
-        for(int i=0 ; i<nums.length ;i++){
-            if(i%2==0){
-                nums[i] = pos[j];
-                j++;
-            }else{
-                nums[i] = neg[k];
-                k++;
-            }
-        }
+        // }
+        // j=0;
+        // k=0;
+        // for(int i=0 ; i<nums.length ;i++){
+        //     if(i%2==0){
+        //         nums[i] = pos[j];
+        //         j++;
+        //     }else{
+        //         nums[i] = neg[k];
+        //         k++;
+        //     }
+        // }
 
-        return nums;
+        // return nums;
     }
 }
