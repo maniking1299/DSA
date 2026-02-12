ed3156ae98c5d7d603abfb3199882d08d29ea581
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-02-12 09:55:16 +0530

    Time: 1 ms (99.62%) | Memory: 77.6 MB (14.29%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,5 +1,9 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+
+        // Pattern - Kadanes Algo
+        // TC -> O(n)
+        // SC -> O(1)
         int maxSum =nums[0] ;
         int sum = 0;
       
