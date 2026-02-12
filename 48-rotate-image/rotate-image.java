3960331abac9ac3feda08b9a79bb9567cceb2240
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-02-12 09:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (69.76%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,21 +1,47 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+
+    // Optmial Solution -> Transpose and reverse it
+
+    // Found the transpose
+    for(int i=0 ; i<n;i++){
+        for(int j=i+1 ; j<n;j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    // Reverse the cols 
+    for(int i=0 ; i<n ;i++){
+        for(int j=0 ;j<n/2 ;j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n-j-1];
+            matrix[i][n-j-1] = temp;
+        }
+    }
+
+ }
+ 
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
         // Brute force way ->
         // TC = O(n^2)
         // SC = O(n^2)
-        int ans[][] = new int[n][n];
-        for(int i=0 ; i<n ;i++){
-            for(int j=0 ; j<n ;j++){
+        // int ans[][] = new int[n][n];
+        // for(int i=0 ; i<n ;i++){
+        //     for(int j=0 ; j<n ;j++){
 
-                ans[j][n-1-i] = matrix[i][j];
-            }
-        }
+        //         ans[j][n-1-i] = matrix[i][j];
+        //     }
+        // }
 
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ;j++){
-                matrix[i][j] = ans[i][j];
-            }
-        }
-    }
-}
+        // for(int i=0 ; i<n ; i++){
+        //     for(int j=0 ; j<n ;j++){
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+////////////////////////////////////////////////////////////////////////////////////////////////////////        
