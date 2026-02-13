672403e9a616746d1e60a3a076c94c12d6f3c3dc
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-02-13 20:02:17 +0530

    Time: 1 ms (99.83%) | Memory: 55.7 MB (60.58%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+// Optimal Solution -> Moore Voting Algorithm ///////////////
+    // TC ->O(N)
+    // SC ->O(1)
+
+    int ans = 0;
+    int freq = 0;
+
+    for(int i=0 ; i<nums.length ;i++){
+        if(freq == 0){
+            ans = nums[i];
+        }
+        if(ans == nums[i]) freq++;
+        else freq--;
+    }
+
+return ans;
+
+
+    }
+}
+
+  /////////////// Better than brute force but not optimal///////////////////////////////////
+
+        // Tc -> O(N)      
+        // Sc -> O(N)      
+        
+        // HashMap <Integer,Integer> hm = new HashMap<>();
+
+        
+        // for(int i=0 ; i<nums.length ; i++){
+        //     int num = nums[i];
+        //     hm.put(num,hm.getOrDefault(num,0)+1);
+        // }
+
+        // for(int k: hm.keySet()){
+        //     if(hm.get(k) > nums.length/2) return k;
+        // }
+        // return -1;
