434f97cff72030655c65dc04008a0aaba41ee6fc
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-02-14 21:43:17 +0530

    Time: 2 ms (97.60%) | Memory: 46.7 MB (43.70%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+
+        // TC -> O(N^2) 
+        // SC -> O(N)
+        
+        StringBuilder sb = new StringBuilder();
+
+        for(String s :words){
+            int sm = sum(s , weights);
+            int idx = sm%26;
+
+            char ch = Findchar(idx);
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+    private int sum(String s , int[] weights){
+        int sum =0;
+        for(int i=0 ;i<s.length();i++){
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+
+            sum += weights[idx];
+        }
+
+        return sum;
+    }
+    private char Findchar(int idx){
+        
+        return (char)('z' - idx );
+    }
+}
+
+
+
+
+
