11358f180a41cbb1ac3946590b29641a88c40665
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-03-15 09:46:16 +0530

    Time: 1 ms (99.03%) | Memory: 42.9 MB (29.83%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,13 +1,15 @@
 class Solution {
     public long countCommas(long n) {
 
-        // TC ->O(1)
+        // TC ->O(N)
         // SC ->O(1)
+        long base = 1000;
+        long count = 0;
 
-        if(n<1000) return 0;
-
-        long total = Math.max(0,n-1000L+1)+ Math.max(0,n-1000000L+1)+ Math.max(0,n-1000000000L+1)+ Math.max(0,n-1000000000000L+1)+ Math.max(0,n-1000000000000000L+1);
-
-        return total;
+        while(base<=n){
+            count = count + (n-base+1);
+            base = base*1000;
+        }
+         return count;
     }
 }
